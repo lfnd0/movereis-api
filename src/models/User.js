ee104b5bd97bb06b2dbc34995ac6766319b48763
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-10 11:27:09 -0300

    Alter model user

--- src/models/User.js
+++ src/models/User.js
@@ -6,6 +6,7 @@ class User extends Model {
     super.init({
       username: DataTypes.STRING,
       email: DataTypes.STRING,
+      password: DataTypes.STRING,
     }, {
       sequelize,
     });
