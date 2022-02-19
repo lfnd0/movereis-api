ae4bc24a93187b2dc680ba2ae426f26c8c9a4365
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-19 16:04:05 -0300

    Alter migration create genre

--- src/database/migrations/20220209174025-create-genres.js
+++ src/database/migrations/20220209174025-create-genres.js
@@ -15,6 +15,10 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
