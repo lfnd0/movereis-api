0d7994e6dfbc5f554278206da28d62f6a555110a
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-19 16:04:30 -0300

    Alter seeders genre types

--- src/database/seeders/20220209215523-genre-types.js
+++ src/database/seeders/20220209215523-genre-types.js
@@ -4,54 +4,67 @@ module.exports = {
       {
         name: "Ação",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Aventura",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Comédia",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Documentário",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Drama",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Faroeste",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Fantasia",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Ficção científica",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Musical",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Policial",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Romance",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Terror",
         created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         name: "Thriller",
         created_at: new Date(),
+        updated_at: new Date(),
       },
     ]);
   },
