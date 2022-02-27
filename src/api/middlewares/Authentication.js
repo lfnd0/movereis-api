8224d89083cb386cf48ffa78a594358371ade5c5
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-27 11:41:44 -0300

    Create authentication middleware

---
+++ src/api/middlewares/Authentication.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const { verify } = require("jsonwebtoken");
+const User = require("../models/User");
+
+module.exports = async (request, response, next) => {
+  const auth = request.headers.authorization;
+
+  if (!auth) {
+    return response.status(404).json({ error: "Token missing." });
+  }
+
+  const [, token] = auth.split(" ");
+
+  try {
+    const user_id = verify(token, process.env.TOKEN_SECRET);
+    const user = await User.findByPk(user_id.sub);
+
+    if (!user) {
+      return response.status(404).json({ error: "User not found." });
+    }
+
+    request.user = user_id.sub;
+
+    return next();
+  } catch {
+    return response.status(401).json({ error: "Invalid token." });
+  }
+};
