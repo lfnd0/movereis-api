50f6b3d1e010b3deed8e1a33cee7e2bf94d22090
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-27 11:43:50 -0300

    Alter genre controller

--- src/api/controllers/GenreController.js
+++ src/api/controllers/GenreController.js
@@ -10,7 +10,7 @@ module.exports = {
     });
 
     if (!genre) {
-      return response.status(400).json({ error: "Genre not found." });
+      return response.status(404).json({ error: "Genre not found." });
     }
 
     return response.status(200).json(genre.movies);
