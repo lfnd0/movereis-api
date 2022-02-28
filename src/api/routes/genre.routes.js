203c306e47179625ed860e85e6780db12ed3830f
Mateus Cardoso <serjumano17@gmail.com>
2022-02-27 23:04:33 -0300

    feat!: added celebrate to validate params of request

--- src/api/routes/genre.routes.js
+++ src/api/routes/genre.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const routes = express.Router();
 const genreController = require("../controllers/GenreController");
 
-routes.get("/:id/list/movies", genreController.listGenresWithMovies);
+routes.get("/:id/list/movies", celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), genreController.listGenresWithMovies);
 
 module.exports = routes;
--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const routes = express.Router();
 const UserController = require("../controllers/UserController");
 const Authentication = require("../middlewares/Authentication");
 
-routes.post("/", UserController.createUser);
+routes.post("/", celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    username: Joi.string().required(),
+    email: Joi.string().required(),
+    password: Joi.string().required()
+  })
+}), UserController.createUser);
 routes.get("/list/movies", Authentication, UserController.listMovies);
-routes.patch("/change/email", Authentication, UserController.changeEmail);
+routes.patch("/change/email", celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().required()
+  })
+}), Authentication, UserController.changeEmail);
 
 module.exports = routes;
