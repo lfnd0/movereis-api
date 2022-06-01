3569bc3837856514bd49168495c38d8103a04f24
luizfernando <luiz-fsilva37@hotmail.com>
2022-05-31 21:31:17 -0300

    Add endpoints documentation

--- src/api/app.js
+++ src/api/app.js
@@ -1,7 +1,11 @@
 require("../database");
+
 const { errors } = require("celebrate");
 
 const express = require("express");
+
+const swaggerUI = require("swagger-ui-express");
+const swaggerFile = require("./swagger.json");
 
 const userRoutes = require("./routes/user.routes");
 const movieRoutes = require("./routes/movie.routes");
@@ -11,6 +15,8 @@ const authenticationRoutes = require("./routes/authentication.routes");
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/users", userRoutes);
 app.use("/movies", movieRoutes);
--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require("./api/app");
 const port = process.env.SERVER_PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Active app at: http://localhost:${port}`);
+  console.log(`Active app at: http://localhost:${port}.\nDocumentation at: http://localhost:${port}/api-docs`);
 });
